f2d8584d99477e5e0d6b6195cfa72122a8070271
Serik1205 <barba_sl@i.ua>
2023-10-05 12:01:00 +0300

    upd1 js-todo-list

--- src/container/index/index.js
+++ src/container/index/index.js
@@ -1,3 +1,38 @@
-import '../../script/test'
+export class Todo {
+	static #list = []
+	static #count = 0
+	static #createTaskData = (text) => {
+		this.#list.push({
+			id: ++this.#count,
+			text,
+			done: false,
+		})
+	}
+	static #block = null
+	static #template = null
+	static #input = null
+	static #button = null
+	static init = () => {
+		this.#template = document.getElementById('task').content.firstElementChild
+		this.#block = document.querySelector('.task__list')
 
-console.log('container')
+		this.#input = document.querySelector('.form__input')
+
+		this.#button = document.querySelector('.form__button')
+		this.#button.onclick = this.#handleAdd
+		this.#render()
+
+	}
+	static #handleAdd = () => {
+		const value = this.#input.value
+		if (value.length > 1) {
+			this.#createTaskData(value)
+			this.#input.value = ''
+			this.#render
+		}
+		console.log(this.#list);
+	}
+	static #render = () => { }
+}
+Todo.init()
+window.todo = Todo
